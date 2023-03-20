7796ac77ded58418d4b4c133a5810b10fd5d2f67
sumintmin <ysumin128@gmail.com>
2023-03-20 13:38:35 +0900

    #test 01

--- Week3/src/math.java
+++ Week3/src/math.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class math {
+public class math {
+    public static void main(String[] args) {
+
+
+        for (int x = -20; x <= 20; x++) {
+            int t = (int)(Math.sin((x*10)*Math.PI/180)*20);
+            for (int i = 0; i < t+20; i++) {
+                System.out.print(' ');
+                System.out.println("helloworld!");
+            }
+            System.out.print('*');
+        }
+    }
 }
