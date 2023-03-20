e6a3aec7fc7916ac4890b9ef4943c3d9bfc8b191
sumintmin <ysumin128@gmail.com>
2023-03-20 13:45:21 +0900

    #class 01

---
+++ Week3/src/DrawEqNoArray.java
@@ -0,0 +1,11 @@
+public class DrawEqNoArray {
+    public static void main(String[] args) {
+        for (int x = -20; x < 20 ; x++) {
+            int t = (int)(Math.sin(x*10.*Math.PI/180.)*20);
+            for (int i = 0; i < t+20; i++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+    }
+}
